d20c745db0858c788b55434ecea3c9c4713fbd20
lunasky-hy <19776220+lunasky-hy@users.noreply.github.com>
2025-12-11 20:57:32 +0900

    DB connection string settig is updated

--- src/database/gorm.go
+++ src/database/gorm.go
@@ -16,13 +16,17 @@ func ConnectPostgres() *gorm.DB {
 	DB_USER := os.Getenv("DB_USER")
 	DB_NAME := os.Getenv("DB_NAME")
 	DB_PORT := os.Getenv("DB_PORT")
+	DB_PASS := os.Getenv("DB_PASSWORD")
+	if DB_PASS == "" {
+		DB_PASS="null"
+	}
 	DB_URL := os.Getenv("DB_URL")
 	DB_PORTNUM, _ := strconv.Atoi(DB_PORT)
 
 	dsn := DB_URL
 	if DB_URL == ""{
-		dsn = fmt.Sprintf("host=%s user=%s dbname=%s port=%d sslmode=disable TimeZone=Asia/Tokyo", 
-			DB_HOST, DB_USER, DB_NAME, DB_PORTNUM)
+		dsn = fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%d sslmode=disable", 
+			DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORTNUM)
 	}
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if (err != nil) {
